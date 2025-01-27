89080707ab9929024cc33f796dc885844e7d4922
khyati2002 <78091360+khyati2002@users.noreply.github.com>
2025-01-28 00:38:53 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,3 +1,12 @@
+const fs = require('fs');
+
+// The string value you want to write
+const content = "Hello, this is the content I want to write to a file!";
+
+// Write to a new file
+fs.writeFileSync('output.txt', content);
+
+console.log('File has been created successfully');
 console.log("Hi");
 const myVar = process.env.My_Variable;
 console.log("Var is " + myVar);
